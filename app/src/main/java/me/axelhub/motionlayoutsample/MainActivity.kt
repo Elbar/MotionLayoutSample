fb25a412e894633a441a3d4ce6c24bb7be890139
bl1tz <Elbar.israilov@gmail.com>
2020-11-25 18:07:30 +0600

    init commit

---
+++ app/src/main/java/me/axelhub/motionlayoutsample/MainActivity.kt
@@ -0,0 +1,14 @@
+package me.axelhub.motionlayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
